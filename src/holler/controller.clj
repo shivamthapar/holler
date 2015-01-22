d77b2f62927bb835492fe5ba2b5853f1ba576307
Shivam Thapar <shivam.thapar1@gmail.com>
2015-01-22 14:59:24 -0800

    dummy post method setup

--- src/holler/controller.clj
+++ src/holler/controller.clj
@@ -3,11 +3,16 @@
   (:require [compojure.core :refer [defroutes GET POST]]
            [clojure.string :as str]
            [ring.util.response :as ring]
+           [ring.middleware.params :only [wrap-params]]
            [holler.views :as view]
            [holler.models :as model]))
 
 (defn index []
   (view/index (model/all)))
 
+(defn sms-handler []
+  ())
+
 (defroutes routes
-  (GET "/" [] (index)))
+  (GET "/" [] (index))
+  (POST "/post" request (str request)))
